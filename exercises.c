67d6c5d855b840766270ec5c303bf8a792a5415f
Your Name <you@example.com>
2024-03-14 15:02:46 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -30,6 +30,10 @@ void reverseArray(int arr[], int size)
     {
       vector[i] = arr[size - i - 1];
     }
+  for (int i = 0; i < size; i++)
+  {
+    arr[i] = vector[i];
+  }
 }
 
 /*
@@ -64,18 +68,18 @@ arreglos en un tercer arreglo también ordenado de menor a mayor.
 */
 void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2, int result[]) 
 {
-  result[size1 + size2];
+  vector[size1 + size2];
   int i = 0, j = 0;
   for(int k = 0 ; k < size1 + size2; k++)
     {
       if (arr1[i] <= arr2[j])
       {
-        result[k] = arr1[i];
+        vector[k] = arr1[i];
         j++;
       }
       else
       {
-        result[k] = arr2[j];
+        vector[k] = arr2[j];
         i++;
       }
     }
