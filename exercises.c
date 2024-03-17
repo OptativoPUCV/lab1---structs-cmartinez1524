c3b9e70618cf784be0a26bdc3d87512815c803e4
Your Name <you@example.com>
2024-03-17 00:03:57 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -134,11 +134,6 @@ void inicializarLibro(Libro *libro, const char *titulo, const char *nombreAutor,
   strcpy(libro->autor.nombre, nombreAutor);
   libro->autor.anioNacimiento = anioNacimiento;
   libro->anioPublicacion = anioPublicacion;
-
-  
-  
-  
-  
 }
 
 /*
@@ -158,4 +153,28 @@ typedef struct nodo {
   struct nodo *siguiente; // puntero al siguiente nodo
 } Nodo;
 
-Nodo *crearListaEnlazada(int arr[], int size) { return NULL; }
+Nodo *crearListaEnlazada(int arr[], int size) 
+{
+  Nodo *cabeza = NULL;
+  Nodo *ultimo = NULL;
+  for (int i = 0; i < size; i++)
+    {
+      Nodo *nuevoNodo = (Nodo *)malloc(sizeof(Nodo));
+      nuevoNodo->numero = arr[i];
+      nuevoNodo->siguiente = NULL;
+      if (cabeza == NULL)
+      {
+        cabeza = nuevoNodo;
+        ultimo = nuevoNodo;
+        
+      }
+      else
+      {
+        ultimo->siguiente = nuevoNodo;
+        ultimo = nuevoNodo;
+      }
+      
+    }
+  return cabeza;
+  
+}
